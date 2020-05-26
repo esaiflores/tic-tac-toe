18e93a05623b21029fea646fc480d117b211110a
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 17:40:38 -0500

    feat: tic tac toe working, but room for improvement

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
+
+static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
     public static void main(String[] args) {
         char [] [] gameBoard = {{' ', '|', ' ', '|', ' '},
                                 {'-', '+', '-', '+', '-'},
@@ -10,42 +14,29 @@ public class TicTacToe {
 
         printGameBoard(gameBoard);
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter which square you would like (1-9) : ");
-        int position = scan.nextInt();
 
-        System.out.println(position);
 
-        switch (position) {
-            case 1:
-                gameBoard[0][0] =  'X';
-                break;
-            case 2:
-                gameBoard[0][2] =  'X';
-                break;
-            case 3:
-                gameBoard[0][4] =  'X';
-                break;
-            case 4:
-                gameBoard[2][0] =  'X';
-                break;
-            case 5:
-                gameBoard[2][2] =  'X';
-                break;
-            case 6:
-                gameBoard[2][4] =  'X';
-                break;
-            case 7:
-                gameBoard[4][0] =  'X';
-                break;
-            case 8:
-                gameBoard[4][2] =  'X';
-                break;
-            case 9:
-                gameBoard[4][4] =  'X';
-                break;
+        while (true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter which square you would like (1-9) : ");
+            int playerPosition = scan.nextInt();
+
+
+            placePiece(gameBoard, playerPosition, "player");
+
+            Random rand = new Random();
+            int cpuPosition = rand.nextInt(9) + 1;
+            placePiece(gameBoard, cpuPosition, "CPU");
+
+            printGameBoard(gameBoard);
+
+            String result = checkWinner();
+            System.out.println(result);
+
         }
-        printGameBoard(gameBoard);
+
+
+
     }
 
     public static void printGameBoard(char[][] gameBoard){
@@ -56,5 +47,82 @@ public class TicTacToe {
             System.out.println();
         }
 
+    }
+
+    public static void placePiece(char[][] gameBoard, int position, String user) {
+        char symbol = ' ';
+
+        if(user.equals("player")) {
+            symbol = 'X';
+            playerPositions.add(position);
+        }else if (user.equals("CPU")) {
+            symbol = 'O';
+            cpuPositions.add(position);
+        }
+
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] =  symbol;
+                break;
+            case 3:
+                gameBoard[0][4] =  symbol;
+                break;
+            case 4:
+                gameBoard[2][0] =  symbol;
+                break;
+            case 5:
+                gameBoard[2][2] =  symbol;
+                break;
+            case 6:
+                gameBoard[2][4] =  symbol;
+                break;
+            case 7:
+                gameBoard[4][0] =  symbol;
+                break;
+            case 8:
+                gameBoard[4][2] =  symbol;
+                break;
+            case 9:
+                gameBoard[4][4] =  symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static String checkWinner(){
+        List topRow = Arrays.asList(1,2,3);
+        List midRow = Arrays.asList(4,5,6);
+        List botRow = Arrays.asList(7,8,9);
+        List leftCol = Arrays.asList(1,4,7);
+        List midCol = Arrays.asList(2,5,8);
+        List rightCol = Arrays.asList(3,6,9);
+        List leftDia = Arrays.asList(1,5,9);
+        List rightDia = Arrays.asList(7,5,3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(leftDia);
+        winning.add(rightDia);
+
+        for(List l : winning ) {
+            if(playerPositions.containsAll(l)) {
+                return "Congratulations you won!";
+            }else if(cpuPositions.containsAll(l)){
+                return "Oof, CPU Wins! Sorry ):";
+            }else if (playerPositions.size() + cpuPositions.size() ==9) {
+                return "CAT!";
+            }
+        }
+
+        return "";
     }
 }
