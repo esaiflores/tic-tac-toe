c0d9a2d560cbf7afd7209ef7cc09f4be9f174478
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 16:56:32 -0500

    created switch case to allow user to place piece

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         char [] [] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -5,5 +7,54 @@ public class TicTacToe {
                                 {' ', '|', ' ', '|', ' '},
                                 {'-', '+', '-', '+', '-'},
                                 {' ', '|', ' ', '|', ' '},};
+
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter which square you would like (1-9) : ");
+        int position = scan.nextInt();
+
+        System.out.println(position);
+
+        switch (position) {
+            case 1:
+                gameBoard[0][0] =  'X';
+                break;
+            case 2:
+                gameBoard[0][2] =  'X';
+                break;
+            case 3:
+                gameBoard[0][4] =  'X';
+                break;
+            case 4:
+                gameBoard[2][0] =  'X';
+                break;
+            case 5:
+                gameBoard[2][2] =  'X';
+                break;
+            case 6:
+                gameBoard[2][4] =  'X';
+                break;
+            case 7:
+                gameBoard[4][0] =  'X';
+                break;
+            case 8:
+                gameBoard[4][2] =  'X';
+                break;
+            case 9:
+                gameBoard[4][4] =  'X';
+                break;
+        }
+        printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char[][] gameBoard){
+        for(char[] row : gameBoard) {
+            for(char c :row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
     }
 }
